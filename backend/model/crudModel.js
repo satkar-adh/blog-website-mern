519efeb77cde86b52977de20c448291c9aea568e
satkar-adh <satkar147258369@gmail.com>
2024-04-24 22:21:36 +0545

    mongodb schema created

---
+++ backend/model/crudModel.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+schema = new mongoose.Schema(
+    {
+        id:{
+            type: Int8Array,
+            unique: True
+        },
+        email:{
+            type: String,
+            required: [true, "Email required"],
+            trim: true,
+            lowercase: true,
+            unique: [true,"Email already exists"]
+        },
+        title:{
+            type: String,
+            required: [true,"Title can't be blank"]
+        },
+        description:{
+            type: String,
+            required: [true,"Description can't be blank"]
+        }
+    }
+);
+
+module.exports = mongoose.model("Crud",schema,"Cruds");
